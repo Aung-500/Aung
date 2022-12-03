5bf03a392dfc2662f4d735ef7defe380858f9841
Aung-500 <1999aungpyaekyaw@gmail.com>
2022-12-03 11:23:08 +0630

    Test

--- main.py
+++ main.py
@@ -4,6 +4,6 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
+
+print("Hello")
